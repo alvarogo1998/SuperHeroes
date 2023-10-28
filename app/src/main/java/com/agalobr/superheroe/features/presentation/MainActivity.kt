0f1ed4f2307137074063f0266ff62b467485cbf8
Alvaro Gallego <rovala18@hotmail.com>
2023-10-28 20:15:14 +0200

    Add Presentation Layer, Skeleton and Errors Views

---
+++ app/src/main/java/com/agalobr/superheroe/features/presentation/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.agalobr.superheroe.features.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.agalobr.superheroe.R
+import com.agalobr.superheroe.app.error.ErrorApp
+import com.agalobr.superheroe.app.extensions.GsonSerialization
+import com.agalobr.superheroe.app.extensions.errorDatabase
+import com.agalobr.superheroe.app.extensions.errorInternet
+import com.agalobr.superheroe.app.extensions.errorUnknown
+import com.agalobr.superheroe.databinding.ActivityMainBinding
+import com.agalobr.superheroe.features.data.ApiClient
+import com.agalobr.superheroe.features.data.biography.BiographyDataRepository
+import com.agalobr.superheroe.features.data.biography.local.XmlBiographyLocalDataSource
+import com.agalobr.superheroe.features.data.biography.remote.api.BiographyDataRemoteSource
+import com.agalobr.superheroe.features.data.superheroe.SuperHeroeDataRepository
+import com.agalobr.superheroe.features.data.superheroe.local.XmlSuperHeroeLocalDataSource
+import com.agalobr.superheroe.features.data.superheroe.remote.api.SuperHeroesDataRemoteSource
+import com.agalobr.superheroe.features.data.work.WorkDataRepository
+import com.agalobr.superheroe.features.data.work.local.XmlWorkLocalDataSource
+import com.agalobr.superheroe.features.data.work.remote.api.WorkDataRemoteSource
+import com.agalobr.superheroe.features.domain.SuperHeroesFeedUseCase
+import com.agalobr.superheroe.features.presentation.adapter.SuperHeroeFeedAdapter
+import com.faltenreich.skeletonlayout.Skeleton
+import com.faltenreich.skeletonlayout.applySkeleton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val skeleton: Skeleton by lazy {
+        binding.listSuperHeroe.applySkeleton(R.layout.view_superhero_feed_item, 10)
+    }
+
+    private val superHeroAdapter = SuperHeroeFeedAdapter()
+
+    private val viewModel: SuperHeroFeedViewModel by lazy {
+        SuperHeroFeedViewModel(
+            SuperHeroesFeedUseCase(
+                WorkDataRepository(
+                    XmlWorkLocalDataSource(this@MainActivity, GsonSerialization()),
+                    WorkDataRemoteSource(ApiClient())
+                ),
+                BiographyDataRepository(
+                    BiographyDataRemoteSource(ApiClient()),
+                    XmlBiographyLocalDataSource(this@MainActivity, GsonSerialization())
+                ),
+                SuperHeroeDataRepository(
+                    SuperHeroesDataRemoteSource(ApiClient()),
+                    XmlSuperHeroeLocalDataSource(this@MainActivity, GsonSerialization())
+                )
+            )
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpObserver()
+        setUpView()
+        viewModel.loadSuperHero()
+
+    }
+
+    private fun setUpObserver() {
+        val observer = Observer<SuperHeroFeedViewModel.UiState> { uiStateSuperHeroFeed ->
+
+            if (uiStateSuperHeroFeed.isLoading) {
+                skeleton.showSkeleton()
+            } else {
+                skeleton.showOriginal()
+                if (uiStateSuperHeroFeed.errorApp != null) {
+                    showError(uiStateSuperHeroFeed.errorApp)
+                } else {
+                    superHeroAdapter.setDataItems(uiStateSuperHeroFeed.superHero)
+                }
+            }
+        }
+        viewModel.uiState.observe(this, observer)
+    }
+
+    private fun setUpView() {
+        binding.apply {
+            listSuperHeroe.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = superHeroAdapter
+            }
+        }
+    }
+
+    private fun showError(error: ErrorApp) {
+        when (error) {
+            ErrorApp.DatabaseErrorApp -> errorDatabase()
+            ErrorApp.InternetErrorApp -> errorInternet()
+            ErrorApp.UnKnownError -> errorUnknown()
+        }
+    }
+
+    private fun errorUnknown() = binding.viewError.errorUnknown()
+
+    private fun errorDatabase() = binding.viewError.errorDatabase()
+
+    private fun errorInternet() = binding.viewError.errorInternet()
+}
---
+++ app/src/main/java/com/agalobr/superheroe/features/presentation/adapter/SuperHeroFeedViewHolder.kt
@@ -0,0 +1,21 @@
+package com.agalobr.superheroe.features.presentation.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.agalobr.superheroe.app.extensions.loadUrl
+import com.agalobr.superheroe.databinding.ViewSuperheroFeedItemBinding
+import com.agalobr.superheroe.features.domain.SuperHeroesFeedUseCase
+
+class SuperHeroFeedViewHolder(binding: ViewSuperheroFeedItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+    private val binding = ViewSuperheroFeedItemBinding.bind(binding.root)
+
+    fun bind(superHero: SuperHeroesFeedUseCase.SuperHeroeList){
+
+        binding.apply {
+            imageSuperheroe.loadUrl(superHero.image)
+            textName.text = superHero.name
+            textFullName.text = superHero.fullName
+            textOccupation.text = superHero.occupation
+        }
+    }
+}
