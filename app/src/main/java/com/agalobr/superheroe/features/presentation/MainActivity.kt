a1ac96a648a16b2ed1594a4c69b309e56270c347
Alvaro Gallego <rovala18@hotmail.com>
2024-01-15 19:13:24 +0100

    Feature/7/Icon And Splash Adding an Icon and Splash to the application

--- app/src/main/java/com/agalobr/superheroe/features/presentation/MainActivity.kt
+++ app/src/main/java/com/agalobr/superheroe/features/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.agalobr.superheroe.features.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.agalobr.superheroe.R
@@ -13,12 +14,14 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
+        screenSplash.setKeepOnScreenCondition { false }
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    fun changeFragment(fragment: Fragment){
+    fun changeFragment(fragment: Fragment) {
         supportFragmentManager.commit {
             replace(R.id.fragment_container, fragment)
             setReorderingAllowed(true)
